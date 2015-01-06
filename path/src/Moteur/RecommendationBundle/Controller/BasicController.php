9e452aea587f969794d5f120cad021d5ae99c39d
MKCG <masseix.kevin@gmail.com>
2015-01-06 12:32:08 +0100

    connexion utilisateur

--- path/src/Moteur/RecommendationBundle/Controller/BasicController.php
+++ path/src/Moteur/RecommendationBundle/Controller/BasicController.php
@@ -14,15 +14,134 @@ use Moteur\ProduitBundle\Model\MotQuery;
 use Moteur\RecommendationBundle\Model\ProfilScoreUtilisateur;
 use Moteur\RecommendationBundle\Model\ProfilScoreUtilisateurQuery;
 use Moteur\ProduitBundle\Dictionnaire\IndexationMot;
+use Moteur\UtilisateurBundle\Geolocalisation\GeoIp;
+
+use Symfony\Component\HttpFoundation\Response;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Moteur\UtilisateurBundle\Model\Ip;
+
 
 class BasicController extends Controller
 {
 	
-     /**
-     * @Route("/search/", name="_search")
-     * @Template()
-     */
+	
+
+	public function indexAction()
+	{
+		include_once 'functions/functions.php';
+	
+		$request = new Request($_GET, $_POST, array(), $_COOKIE, $_FILES, $_SERVER);
+	
+		$path =  $this->get('kernel')->locateResource("@MoteurRecommendationBundle/Data/data.txt");
+		$data = tokenization(utf8_decode(file_get_contents($path)),"\n",0,1);
+		$url = "http://it-ebooks-api.info/v1/search/";
+		$parsed_json['Total'] = "0";
+	
+		while ($parsed_json['Total'] == "0"){
+			$query= rtrim($data[rand(0,count($data)-1)]);
+			 
+			$json = file_get_contents($url.$query);
+			$parsed_json = json_decode($json,true);
+	
+		}
+		//echo $parsed_json['Total']/10;
+	
+		$books = count($parsed_json['Books']);
+	
+		 
+		$response = new Response();
+		//$response->headers->clearCookie('cookie');
+		//$response->send();
+	
+		$dejaVu = $request->cookies->has('cookie');
+	
+		//$request->cookies->get("mycookie");
+		$session = new Session();
+		
+
+		$recommandation_books = null;
+	
+		if($dejaVu){
+			if($session->get('id')){
+				$user = UtilisateurQuery::create()
+				->filterById($session->get('id'))
+				->findOne();
+	
+				$recommandation_books = recommandations_articles(recommandation_description($data,$user->getDescription()));
+			}
+			$flag = false;
+			 
+		}else {
+			if($request->getMethod() == 'POST')
+			{
+				$session->start();
+	
+				// définit des messages dits « flash »
+				$session->getFlashBag()->add('notice', 'Utilisateur Modifier');
+				$session->getFlashBag()->add('error', 'Pas d\'utilisateur');
+				 
+				$nom = $_POST['nom'];
+				$prenom = $_POST['prenom'];
+				$age = $_POST['age'];
+				$ville = $_POST['ville'];
+				$description = $_POST['description'];
+	
+				// définit et récupère des attributs de session
+	
+				$geo = new GeoIp();
+				
+				do{
+					$geo->setIpadress(rand(0,255).".".rand(0,255).".".rand(0,255).".".rand(0,255));
+					$geo->geoCheckIP();
+				}
+				while($geo->pays==null);
+				
+				$ip = new Ip();
+				$ip->setPays($geo->pays);
+				$ip->setDepartement($geo->departement);
+				$ip->setVille($geo->ville);
+				$ip->save();
+				
+				$utilisateur = new Utilisateur();
+				$utilisateur->setNom($nom);
+				$utilisateur->setPrenom($prenom);
+				$utilisateur->setVille($ville);
+				$utilisateur->setAge($age);
+				$utilisateur->setDescription($description);
+				$utilisateur->setIpUtilisateur($this->container->get('request')->getClientIp());
+				$utilisateur->setIp();
+				$utilisateur->save();
+	
+				
+	
+				$session->set('id',$utilisateur->getId());// je pense qu'il faut le récuperer de la base ou de l'action précedante
+	
+	
+				unset($_POST);
+	
+				$cookie = new Cookie('cookie', 'utilisateur',time() + 3600 * 24 * 7);
+				$response->headers->setCookie($cookie);
+				$response->send();
+	
+	
+			}
+			$flag=true;
+		}
+	
+		//return $this->render('UserBundle:User:index.html.twig');
+		return $this->render('MoteurRecommendationBundle:User:index.html.twig',array('nb_books' => $books,
+				'books' => $parsed_json['Books'],'flag'=>$flag,'recommandation_book'=>$recommandation_books));
+	}
+	
+	
+    /**
+	 * @Route("/search/", name="_search")
+	 * @Template()
+	 */
     public function rechercherAction()
     {  
     	$request = new Request($_GET, $_POST, array(), $_COOKIE, $_FILES, $_SERVER);
@@ -40,17 +159,13 @@ class BasicController extends Controller
                 $url = "http://it-ebooks-api.info/v1/search/";
                 $json = file_get_contents($url.$_POST['tags']);
                 $parsed_json = json_decode($json,true);
-    		$requete = $_POST['tags'];
+    			$requete = $_POST['tags'];
     		
-                /*if ($parsed_json['Total'] == "0"){
-        
+               /*if ($parsed_json['Total'] == "0"){
                 return new Response('Nothing Found!');
-        
-        
                 }*/
         
     		$indexation = new IndexationMot($requete, $path);
-    		 
     		$requete_id = RequeteQuery::create()->limit(1)->orderBy('requete_id', 'DESC')->findOne()->getRequeteId();
     		$utilisateur_id = $session->get('id');
     		
@@ -83,7 +198,7 @@ class BasicController extends Controller
     		 
     	}  
         $books = count($parsed_json['Books']);
-        return $this->render('UserBundle:User:index.html.twig',array('nb_books' => $books,'books' => $parsed_json['Books'],'flag'=>false));
+        return $this->render('MoteurRecommendationBundle:User:index.html.twig',array('nb_books' => $books,'books' => $parsed_json['Books'],'flag'=>false));
         //return $this->render('MoteurRecommendationBundle:Recherche:liste.html.twig', array('resultats' => $resultat, 'requete' => $requete));
     }
     
@@ -93,7 +208,7 @@ class BasicController extends Controller
      * @param unknown $nombre le nombre de produits � afficher par page
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function indexAction($page, $nombre){
+    public function listeAction($page, $nombre){
     	//Un objet requ�te de Symfony avec les variables globales
     	$request = new Request($_GET, $_POST, array(), $_COOKIE, $_FILES, $_SERVER);
     	
