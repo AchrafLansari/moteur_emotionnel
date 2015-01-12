3f311cf0b59fcf86949a2246336ee542e4dad447
MKCG <masseix.kevin@gmail.com>
2015-01-12 12:16:11 +0100

    Fin suppression Acme (Kernel+Routing)

--- path/app/AppKernel.php
+++ path/app/AppKernel.php
@@ -16,7 +16,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new Acme\UserBundle\UserBundle(),
             new Propel\PropelBundle\PropelBundle(),
             new Moteur\RecommendationBundle\MoteurRecommendationBundle(),
             new Moteur\UtilisateurBundle\MoteurUtilisateurBundle(),
@@ -24,7 +23,6 @@ class AppKernel extends Kernel
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            $bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
